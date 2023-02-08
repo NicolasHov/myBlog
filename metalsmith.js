84ab5c166866281bbf8a1b20bfa18eeae2a56c61
NicolasHov <hovart.nicolas@gmail.com>
2023-02-08 12:50:00 +0100

    Update the default Metalsmith.source() directory to docs

--- metalsmith.js
+++ metalsmith.js
@@ -52,7 +52,7 @@ const templateConfig = {
 function msBuild() {
   Metalsmith(__dirname)
     .source("./src/content")
-    .destination("./build")
+    .destination("docs  ")
     .clean(true)
     .metadata({
       msVersion: dependencies.metalsmith,
