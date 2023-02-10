cc18b4637d281290e61281a2ec65f50ee8e96c55
NicolasHov <hovart.nicolas@gmail.com>
2023-02-10 18:07:54 +0100

    dsadsadsadsdssas

--- metalsmith.js
+++ metalsmith.js
@@ -52,7 +52,7 @@ const templateConfig = {
 function msBuild() {
   Metalsmith(__dirname)
     .source("./src/content")
-    .destination("build")
+    .destination("./build")
     .clean(true)
     .metadata({
       msVersion: dependencies.metalsmith,
